1ec46449570727b08fe25e9210a41f0978acd9db
adityavkulkarni <adityavkulkarni0999@gmail.com>
2024-04-29 21:19:49 -0500

    Added keras RNN and dataprocessor

--- data_processor.py
+++ data_processor.py
@@ -266,13 +266,15 @@ class DataProcessor:
         return x_tk.texts_to_sequences(x), x_tk
 
     @staticmethod
-    def pad(x, length):
+    def pad(x, length=None):
         """
         Pad x
         :param x: List of sequences.
         :param length: Length to pad the sequence to.  If None, use length of longest sequence in x.
         :return: Padded numpy array of sequences
         """
+        if length == None:
+            length = max([len(sentence) for sentence in x])
         return pad_sequences(x, maxlen=length, padding='post')
 
     def preprocess(self, x, y):
