a24d66714f28d083d48541ff0ce8bbac5c08d6ff
adityavkulkarni <adityavkulkarni0999@gmail.com>
2024-05-06 12:57:42 -0500

    misc

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import urllib.request
 
@@ -9,16 +10,25 @@ DATA_PATH = "data/spa.txt"
 DATA_URL = "https://github.com/adityavkulkarni/6375_Project_RNN/raw/master/data/spa.txt"
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Script to train a neural network.')
+    parser.add_argument('--learning-rate', type=float,
+                        help='Learning rate for model', default=0.00001)
+    parser.add_argument('--epochs', type=int,
+                        help='Epochs for model', default=50)
+    args = parser.parse_args()
+
     if not os.path.exists(DATA_PATH):
         urllib.request.urlretrieve(DATA_URL, DATA_PATH)
-    data_processor = DataProcessor(DATA_PATH, sentence_count=1000)
+    data_processor = DataProcessor(DATA_PATH, sentence_count=10000)
     index = 10
     eng_sentence = data_processor.english_sentences[index]
     spa_sentence = data_processor.spanish_sentences[index]
 
     rnn = RNN(input_shape=(data_processor.max_sentence_length, 1),
               output_shape=data_processor.spanish_vocab_len)
-    rnn.train(data_processor.eng_pad_sentence, data_processor.spa_pad_sentence)
+    rnn.train(data_processor.eng_pad_sentence,
+              data_processor.spa_pad_sentence,
+              epochs=args.epochs, learning_rate=args.learning_rate)
 
     print(f"English sentence: {eng_sentence}")
     print(f"Spanish sentence: {spa_sentence}")
@@ -27,7 +37,8 @@ if __name__ == '__main__':
 
     rnn_benchmark = RNNBenchmark(input_shape=(data_processor.max_sentence_length, 1),
                                  output_shape=data_processor.spanish_vocab_len)
-    rnn_benchmark.train(data_processor.eng_pad_sentence, data_processor.spa_pad_sentence)
+    rnn_benchmark.train(data_processor.eng_pad_sentence, data_processor.spa_pad_sentence,
+                        epochs=args.epochs)
     print(f"English sentence: {eng_sentence}")
     print(f"Spanish sentence: {spa_sentence}")
     print(f"Predicted sentence: "
--- model/rnn.py
+++ model/rnn.py
@@ -1,7 +1,9 @@
 import numpy as np
+import pandas as pd
 from keras.src.losses import SparseCategoricalCrossentropy
 from keras.src.metrics import Accuracy
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 from model.layers.input import Input
 from model.layers.recurrent import Recurrent
@@ -16,10 +18,10 @@ class RNN:
         self.dense = Dense(input_shape, output_shape)
 
     def train(self, x, y, epochs=10, learning_rate=0.0001):
+        results = []
+        graph = []
         X_train, X_test, y, y_test = train_test_split(x, y, test_size=0.25, random_state=42)
         for epoch in range(epochs):
-            l_epoch = 0
-            a = 0
             for i in range(len(X_train)):
                 # Forward Pass
                 _y = self.predict(X_train[i])
@@ -36,7 +38,23 @@ class RNN:
             scce = SparseCategoricalCrossentropy()
             err = scce(y_test, _y)
             acc = m.result()
+            results.append((epoch+1, float(acc), float(err)))
+            graph.append((epoch+1, float(err)))
             print(f"epoch: {epoch + 1} Loss: {err} Accuracy: {acc}")
+
+        pd.DataFrame(results, columns=["Epochs", "Accuracy", "Error"]).to_csv(f'results/rnn_{learning_rate}.csv',
+                                                                              index=False)
+        df = pd.DataFrame(graph, columns=["Epochs", "Validation loss"])
+        fig1 = plt.figure(figsize=(12, 7))
+        ax = fig1.add_subplot(1, 1, 1)
+        ax.plot(df["Epochs"], df["Validation loss"])
+        plt.legend(["Validation Loss"])
+        plt.title("Loss vs Epochs")
+        txt = f"Learning Rate: {learning_rate}"
+        plt.figtext(0.5, 0.01,
+                    txt + "\nFinal Loss:{:.2f}".format(graph[-1][1]),
+                    wrap=True, horizontalalignment='center', fontsize=10)
+        fig1.savefig(f"results/loss_{learning_rate}.png")
 
     def predict(self, x):
         y_1 = self.input_layer.forward(x)
